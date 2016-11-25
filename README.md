5cfae6b6e8cf152f18a8a9062b06acaede8104d1
zacacollier <zacacollier@gmail.com>
2016-11-25 13:03:06 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Enter an IP address to remove an entry from ~/.ssh/known_hosts on macOS
---
+++ remove_known_host.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+echo "Enter the IP address (or type 'list' to view known hosts):"
+read input
+case $input in
+    list ) cat ~/.ssh/known_hosts
+        ;;
+    *      ) echo "Are you sure you want to remove $input [y/n]?"
+    read confirm
+    case $confirm in
+        [y] | [Y] ) sed -i "" "/^[^[:digit:]]*$input[^[:digit:]]/d" ~/.ssh/known_hosts
+                    echo "$input was removed from known hosts."
+            ;;
+        * )   echo "Cancelled: IP was not removed"
+            ;;
+    esac
+esac
